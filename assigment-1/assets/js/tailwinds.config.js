97e5c95bf833005a722a2be30696b1c2a6f4cec1
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-10-17 23:49:38 +0700

    feat: create landing page

---
+++ assigment-1/assets/js/tailwinds.config.js
@@ -0,0 +1,27 @@
+// Tailwinds Configuration File
+// Documentation: https://tailwindcss.com/docs/configuration
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+        },
+        secondary: {
+          100: '#F9FAFB',
+          200: '#F3F4F6',
+          300: '#E5E7EB',
+          400: '#D1D5DB',
+        },
+        background: {
+          primary: '#181818',
+        },
+      },
+    },
+  },
+};
